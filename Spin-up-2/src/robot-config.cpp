261d8430b97cf579a5ae135787e164c87651e25f
Luke <62680441+hawslc@users.noreply.github.com>
2022-06-13 12:26:46 -0500

    Add files via upload

---
+++ Spin-up-2/src/robot-config.cpp
@@ -0,0 +1,27 @@
+#include "vex.h"
+
+using namespace vex;
+using signature = vision::signature;
+using code = vision::code;
+
+// A global instance of brain used for printing to the V5 Brain screen
+brain  Brain;
+
+// VEXcode device constructors
+controller Controller1 = controller(primary);
+motor FlywheelMotorA = motor(PORT1, ratio6_1, true);
+motor FlywheelMotorB = motor(PORT2, ratio6_1, false);
+motor_group Flywheel = motor_group(FlywheelMotorA, FlywheelMotorB);
+
+// VEXcode generated functions
+// define variable for remote controller enable/disable
+bool RemoteControlCodeEnabled = true;
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
+ * 
+ * This should be called at the start of your int main function.
+ */
+void vexcodeInit( void ) {
+  // nothing to initialize
+}
